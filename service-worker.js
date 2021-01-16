d5255a850fd8c4a8820e51d5fa175890a236359c
Arkaraj Ghosh <55324916+Arkaraj@users.noreply.github.com>
2021-01-16 17:00:49 +0530

    Added pwa 🎉✨

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+// npx pwa-asset-generator ./Images/mazeicnb.png icons
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
+// This will work!
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
+);
